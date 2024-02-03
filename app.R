259568734434115591e2cd73131a9b4ad18aa87c
Swietlon <44343513+Swietlon@users.noreply.github.com>
2024-02-03 21:34:25 +0100

    Update app.R

--- app.R
+++ app.R
@@ -7,6 +7,7 @@ library(sf)
 library(ggplot2)
 library(plotly)
 library(DT)
+library(stringr)
 
 
 #Ładowanie zbioru danych ####
@@ -84,36 +85,7 @@ ui <- fluidPage(
           p('Ilość poszczególnych kategorii'),
           plotlyOutput(outputId = 'categoryBarPlot'),
           h3('Przegląd gatunków'),
-          selectInput('category',
-                      'Kategoria',
-                      choices = add_row(unique(species['Category']),
-                                        Category = 'All',
-                                        .before = 1)),
-          selectInput('occurrence',
-                      'Występowanie',
-                      choices = drop_na(add_row(unique(species['Occurrence']),
-                                                Occurrence = 'All',
-                                                .before = 1))),
-          selectInput('nativeness',
-                      'Rodzimość',
-                      choices = drop_na(add_row(unique(species['Nativeness']),
-                                                Nativeness = 'All',
-                                                .before = 1))),
-          selectInput('abundance',
-                      'Rzadkość',
-                      choices = drop_na(add_row(unique(species['Abundance']),
-                                                Abundance = 'All',
-                                                .before = 1))),
-          selectInput('seasonality',
-                      'Sezonowość',
-                      choices = drop_na(add_row(unique(species['Seasonality']),
-                                                Seasonality = 'All',
-                                                .before = 1))),
-          selectInput('safeStatus',
-                      'Status',
-                      choices = drop_na(add_row(unique(species['Conservation Status']),
-                                                `Conservation Status` = c('All', 'Safe'),
-                                                .before = 1))),
+          htmlOutput(outputId = 'tableDataInfo'),
           DT::dataTableOutput('spieciesTable'),
           align = 'center',
           class = 'infodiv'
@@ -155,6 +127,7 @@ ui <- fluidPage(
 
 #Serwer ####
 server <- function(input, output) {
+  
   #Informacje ####
   
   na_data <- apply(X = is.na(myData), MARGIN = 2, FUN = mean) * 100
@@ -190,26 +163,46 @@ server <- function(input, output) {
       )
   )
   
+  concatenateStringVector <- function(stringVector) {
+    return(
+      unlist(stringVector) %>%
+        unique() %>%
+        na.omit() %>%
+        paste(collapse = ', ')
+      )
+  }
+  
+  output$tableDataInfo <- renderText({
+    paste(
+      'Wartości występujące w poszczególnych kolumnach:<br>',
+      '<b>Category:</b> ', concatenateStringVector(species['Category']),
+      '<br><b>Occurrence:</b> ', concatenateStringVector(species['Occurrence']),
+      '<br><b>Nativenes:</b> ', concatenateStringVector(species['Nativeness']),
+      '<br><b>Abundance: </b> ', concatenateStringVector(species['Abundance']),
+      '<br><b>Status:</b> ', concatenateStringVector(species['Status'])
+      )
+  })
+  
   output$spieciesTable <- DT::renderDataTable({
     speciesInTable <- species
-    if (input$category != 'All') {
-      speciesInTable <- filter(speciesInTable, Category == input$category)
-    }
-    if (input$occurrence != 'All') {
-      speciesInTable <- filter(speciesInTable, Occurrence == input$occurrence)
-    }
-    if (input$nativeness != 'All') {
-      speciesInTable <- filter(speciesInTable, Nativeness == input$nativeness)
-    }
-    if (input$abundance != 'All') {
-      speciesInTable <- filter(speciesInTable, Abundance == input$abundance)
-    }
-    if (input$seasonality != 'All') {
-      speciesInTable <- filter(speciesInTable, Seasonality == input$seasonality)
-    }
-    if (input$safeStatus != 'All') {
-      speciesInTable <- filter(speciesInTable, Status == input$safeStatus)
-    }
+    # if (input$category != 'All') {
+    #   speciesInTable <- filter(speciesInTable, Category == input$category)
+    # }
+    # if (input$occurrence != 'All') {
+    #   speciesInTable <- filter(speciesInTable, Occurrence == input$occurrence)
+    # }
+    # if (input$nativeness != 'All') {
+    #   speciesInTable <- filter(speciesInTable, Nativeness == input$nativeness)
+    # }
+    # if (input$abundance != 'All') {
+    #   speciesInTable <- filter(speciesInTable, Abundance == input$abundance)
+    # }
+    # if (input$seasonality != 'All') {
+    #   speciesInTable <- filter(speciesInTable, Seasonality == input$seasonality)
+    # }
+    # if (input$safeStatus != 'All') {
+    #   speciesInTable <- filter(speciesInTable, Status == input$safeStatus)
+    # }
     speciesInTable %>%
       select(Category, Order, Family, `Scientific Name`, `Common Names`,
              Occurrence, Nativeness, Abundance, Seasonality, Status) %>%
@@ -363,5 +356,3 @@ server <- function(input, output) {
 
 # Uruchomienie apki ####
 shinyApp(ui = ui, server = server)
-
-
